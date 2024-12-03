2d9273941c2228611881036942bb16cbcd1011e8
dadev123 <dadev123@github.com>
2024-12-03 16:12:56 +0100

    Added Calccameradir something

--- include/al/util.hpp
+++ include/al/util.hpp
@@ -425,6 +425,8 @@ namespace al
     bool isInDeathArea(al::LiveActor const*);
 
     void calcCameraUpDir(sead::Vector3f*, al::IUseCamera const*, int);
+    
+    void calcCameraFront(sead::Vector3f *, al::IUseCamera const*, int);
 
     const unsigned char* tryGetBymlFromArcName(sead::SafeStringBase<char> const&, sead::SafeStringBase<char> const&);
 
