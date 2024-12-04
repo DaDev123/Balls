17990d83dc045757ae28b42dc1d7e605dfc1bc68
dadev123 <dadev123@github.com>
2024-12-04 13:04:41 +0100

    Moon Collect not working, might fix

--- include/rs/util.hpp
+++ include/rs/util.hpp
@@ -44,6 +44,8 @@ namespace rs {
     PlayerActorBase * getPlayerActor(const al::Scene *);
 
     void get2DAreaPos(sead::Vector3<f32> *, al::AreaObj const *);
+    
+    bool isInvalidChangeStage(al::LiveActor const*);
 
     bool isInChangeStageArea(PlayerActorHakoniwa const*, sead::Vector3f const *);
 
@@ -88,4 +90,6 @@ namespace rs {
     int getStageShineAnimFrame(al::LiveActor const*, char const*);
     const char* getStageShineArchiveName(al::LiveActor const*, char const*);
     const char* getStageShineEmptyArchiveName(al::LiveActor const*, char const*);
+    
+    bool isActionEndDemoPlayer(al::LiveActor const*);
 }
